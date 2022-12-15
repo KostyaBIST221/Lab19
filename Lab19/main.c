2370d0feca24a49d43348d16c722a262f9f30c1f
KostyaBIST221 <ksapov24@mail.ru>
2022-12-15 14:58:15 +0300

    1

--- Lab19/main.c
+++ Lab19/main.c
@@ -1,30 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct {
-    char filmname [20];
-    char genre [20];
-    int day;
-    int month;
-    int year;
-    int hour;
-    int minute;
-    int budg;
-} kinoseans;
-
-int writefile (kinoseans * kino, int size){
-    FILE *out;
-    char fname [20] = "st. txt";
-    if ((out = fopen (fname, "wt")) == NULL)
-    {
-        printf("Ошибка открытия файла для записи");
-    }
-        
-        for (int i = 0; i < size; i++) // цикл по всем записям
-            fprintf(out," Имя:%20 ; Жанр:%20 ; Дата:%2d:%2d:%4d; Время:%2d:%2d; Бюджет: %d\n",kino[i].filmname, kino[i].genre, kino[i].day, kino[i].month,kino[i].year, kino[i].hour, kino[i].minute, kino[i].budg) ; // запись полей структуры
-        fclose (out);
-        return 1;
-    }
     
 int creat_file(){
     char fname [20] = "count. txt"; // имя файла
@@ -80,7 +55,6 @@ int main (void)
                 edit_file();
                 break;
             case 3:
-//                writefile(st, <#kinoseans *kino#>, <#int size#>)();
                 break;
             default:
                 break;
